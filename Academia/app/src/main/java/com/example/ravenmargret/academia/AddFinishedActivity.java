a4a32516a2717f84310bc70270b0ba21da2ead96
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2016-07-19 12:31:01 -0400

    Bottom bar working but doing weird things

--- Academia/app/src/main/java/com/example/ravenmargret/academia/AddFinishedActivity.java
+++ Academia/app/src/main/java/com/example/ravenmargret/academia/AddFinishedActivity.java
@@ -5,6 +5,7 @@ import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
@@ -16,7 +17,6 @@ import com.roughike.bottombar.OnTabClickListener;
 public class AddFinishedActivity extends AppCompatActivity
 {
     FragmentManager manager;
-    private BottomBar mBottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -28,48 +28,7 @@ public class AddFinishedActivity extends AppCompatActivity
 
         AddFinishedFragment addFinishedFragment = new AddFinishedFragment();
         showFragment(addFinishedFragment);
-
-        mBottomBar = BottomBar.attach(this, savedInstanceState);
-        mBottomBar.setItems(R.menu.bottombar_menu);
-        mBottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener()
-        {
-            @Override
-            public void onMenuTabSelected(@IdRes int menuItemId)
-            {
-                if (menuItemId == R.id.action_house)
-                {
-                    // The user selected item number one.
-                }
-            }
-
-            @Override
-            public void onMenuTabReSelected(@IdRes int menuItemId)
-            {
-                if (menuItemId == R.id.action_house)
-                {
-                    // The user reselected item number one, scroll your content to top.
-                }
-            }
-        });
-
-//        @Override
-//        protected void onSaveInstanceState(Bundle outstate)
-//        {
-//            super.onSaveInstanceState(outState);
-//            mBottomBar.onSaveInstanceState(outState);
-//        }
-
-        // Setting colors for different tabs when there's more than three of them.
-        // You can set colors for tabs in three different ways as shown below.
-//        mBottomBar.mapColorForTab(0, ContextCompat.getColor(this, R.color.colorAccent));
-//        mBottomBar.mapColorForTab(1, 0xFF5D4037);
-//        mBottomBar.mapColorForTab(2, "#7B1FA2");
-//        mBottomBar.mapColorForTab(3, "#FF5252");
-//        mBottomBar.mapColorForTab(4, "#FF9800");
-
     }
-
-
 
     private void showFragment(Fragment addFinishedFrag)
     {
--- Academia/app/src/main/java/com/example/ravenmargret/academia/HomeActivity.java
+++ Academia/app/src/main/java/com/example/ravenmargret/academia/HomeActivity.java
@@ -3,14 +3,20 @@ package com.example.ravenmargret.academia;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabClickListener;
 
 
 public class HomeActivity extends AppCompatActivity
 {
     FragmentManager manager;
+    private BottomBar mBottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,8 +26,36 @@ public class HomeActivity extends AppCompatActivity
 
         manager = getFragmentManager();
 
-        HomeFragment homeFragment = new HomeFragment();
-        showFragment(homeFragment);
+        AddFinishedFragment addFinishedFragment = new AddFinishedFragment();
+        showFragment(addFinishedFragment);
+
+        mBottomBar = BottomBar.attach(this, savedInstanceState);
+        mBottomBar.setItems(R.menu.bottombar_menu);
+        mBottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+                if (menuItemId == R.id.action_home) {
+                    // The user selected item number one.
+                }
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+                if (menuItemId == R.id.action_home) {
+                    // The user reselected item number one, scroll your content to top.
+                }
+            }
+        });
+
+//        mBottomBar = BottomBar.attachShy((CoordinatorLayout) findViewById(R.id.myCoordinator),
+//                findViewById(R.id.myScrollingContent), savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        mBottomBar.onSaveInstanceState(outState);
     }
 
     private void showFragment(Fragment homeFrag)
