a9004be0b43bb1674078dc1bd79f6c513d9a7cbc
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2016-07-20 17:03:42 -0400

    Got rid of error with intents. Activity not showing

--- Academia/app/src/main/java/com/example/ravenmargret/academia/HomeActivity.java
+++ Academia/app/src/main/java/com/example/ravenmargret/academia/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.ravenmargret.academia;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.design.widget.CoordinatorLayout;
@@ -26,23 +27,60 @@ public class HomeActivity extends AppCompatActivity
 
         manager = getFragmentManager();
 
-        AddFinishedFragment addFinishedFragment = new AddFinishedFragment();
-        showFragment(addFinishedFragment);
+        HomeFragment homeFragment = new HomeFragment();
+        showFragment(homeFragment);
 
         mBottomBar = BottomBar.attach(this, savedInstanceState);
         mBottomBar.setItems(R.menu.bottombar_menu);
-        mBottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
+        mBottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener()
+        {
             @Override
-            public void onMenuTabSelected(@IdRes int menuItemId) {
-                if (menuItemId == R.id.action_home) {
-                    // The user selected item number one.
+            public void onMenuTabSelected(@IdRes int menuItemId)
+            {
+                if (menuItemId == R.id.action_home)
+                {
+                    Intent homeIntent = new Intent(HomeActivity.this, HomeActivity.class);
+                    startActivity(homeIntent);
+                }
+                else if(menuItemId == R.id.action_on_going)
+                {
+                    Intent onGoingIntent = new Intent(HomeActivity.this, OnGoingListActivity.class);
+                    startActivity(onGoingIntent);
+                }
+                else if (menuItemId == R.id.action_finished)
+                {
+                    Intent finishedIntent = new Intent(HomeActivity.this, FinishedListActivity.class);
+                    startActivity(finishedIntent);
+                }
+                else if (menuItemId == R.id.action_grade_scale)
+                {
+                    Intent gradeScaleIntent = new Intent(HomeActivity.this, GradeScaleActivity.class);
+                    startActivity(gradeScaleIntent);
                 }
             }
 
             @Override
-            public void onMenuTabReSelected(@IdRes int menuItemId) {
-                if (menuItemId == R.id.action_home) {
-                    // The user reselected item number one, scroll your content to top.
+            public void onMenuTabReSelected(@IdRes int menuItemId)
+            {
+                if (menuItemId == R.id.action_home)
+                {
+                    Intent homeIntent2 = new Intent(HomeActivity.this, HomeActivity.class);
+                    startActivity(homeIntent2);
+                }
+                else if(menuItemId == R.id.action_on_going)
+                {
+                    Intent onGoingIntent2 = new Intent(HomeActivity.this, OnGoingListActivity.class);
+                    startActivity(onGoingIntent2);
+                }
+                else if (menuItemId == R.id.action_finished)
+                {
+                    Intent finishedIntent2 = new Intent(HomeActivity.this, FinishedListActivity.class);
+                    startActivity(finishedIntent2);
+                }
+                else if (menuItemId == R.id.action_grade_scale)
+                {
+                    Intent gradeScaleIntent2 = new Intent(HomeActivity.this, GradeScaleActivity.class);
+                    startActivity(gradeScaleIntent2);
                 }
             }
         });
